b087efdc38e0e62ee39e3d55ffcea8334b26ee54
Nick Bradley <nick.bradley.193@pm.me>
2018-04-12 04:50:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
